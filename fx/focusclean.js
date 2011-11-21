1b83d0e67e102290d88f4aabe51a27d4ec79fd32
liuyonghui <sdjllyh@gmail.com>
2011-11-21 16:59:31 +0800

    修改fx/focusclean.js 让提示文字为灰色

--- fx/focusclean.js
+++ fx/focusclean.js
@@ -1,15 +1,22 @@
+ZARK_FX.focusclean = {};
+ZARK_FX.focusclean.tip_color = '#CDCDCD';
+
 $('input['+ZARK_FX.FX+']').each(function(){
     var $this = $(this);
     if (ZARK_FX.parserFx($this.attr(ZARK_FX.FX)).focusclean !== undefined){
         var old_value = $this.val();
+        var old_color = $this.css('color');
         $this.focus(function(){
             if($this.val()==old_value){
                 $this.val('');
+                $this.css('color',old_color);
             };
         }).blur(function(){
             if($this.val()==''){
                 $this.val(old_value);
+                $this.css('color',ZARK_FX.focusclean.tip_color);
             };
         });
+        $this.css('color',ZARK_FX.focusclean.tip_color);
     };
 });
