cfcb221d617ada82e97cdeecb368d16413257ea0
liuyonghui <sdjllyh@gmail.com>
2012-03-09 17:41:06 +0800

    修改datepicker支持年月选择

--- fx/datepicker.js
+++ fx/datepicker.js
@@ -29,9 +29,20 @@ ZARK_FX.getFrame('jquery-1.3.2', function($){
             ZARK_FX.getCSS(ZARK_FX.CSS_PATH + 'jqueryui/jqueryui-eggplant/jquery-ui-1.8.16.custom.css');
         };
 
+        if (attrs.language === 'chinese'){
+            attrs.monthNamesShort = ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'];
+        };
+
         var $this = $(this);
-        $this.datepicker({ dateFormat: 'yy-mm-dd', defaultDate: $this.val() })
+        attrs.defaultDate = $this.val();
+        $this.datepicker(attrs)
     
-    }, {style: 'none'}, 'jqueryui-1.8.14' );
+    }, {
+
+        style: 'none',
+        dateFormat: 'yy-mm-dd',
+        language: 'english'
+
+    }, 'jqueryui-1.8.14' );
 
 });
