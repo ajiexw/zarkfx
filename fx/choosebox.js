ad1daa891dea2d13dcd2d8a9b9d47276fe7ab616
liuyonghui <sdjllyh@gmail.com>
2012-03-23 14:22:38 +0800

    add choosebox

---
+++ fx/choosebox.js
@@ -0,0 +1,61 @@
+ZARK_FX.getFrame('jquery-1.3.2', function($){
+
+    var openBox = function(attrs, $value){
+        var $this = $('#'+attrs.boxid);
+
+        $this.css('z-index', attrs.zindex);
+        $this.css('position', 'fixed');
+        $this.css('top', (window.screen.availHeight - $this.height())/2);
+        $this.css('left', (window.screen.availWidth - $this.width())/2);
+        $this.show();
+
+        $(attrs.choice ,$this).click(function(){
+            var val = $(this).val();
+            if (!val){
+                val = $(this).html();
+            };
+            $value.val(val);
+            $this.hide();
+            $value.focus();
+        });
+    
+        if(attrs.cancel){
+            $(attrs.cancel).unbind('click').click(function(){
+                $this.hide();
+            });
+        };
+
+    };
+
+    ZARK_FX.run('choosebox', function(attrs){
+        var $this = $(this);
+        var $switch;
+
+        if (attrs.switchid !== undefined){
+            $switch = $('#'+attrs.switchid);
+        }else{
+            $switch = $this;
+        };
+
+        if (attrs.boxid !== undefined){
+            $switch.click(function(){
+                openBox(attrs, $this);
+            });
+        }else{
+            console.warn('Zark Fx choosebox: miss boxid, ignore.');
+        };
+
+
+    }, {
+        switchid: undefined,
+        boxid: undefined,
+        choice:     'a',
+        overlay:   false,
+        zindex:     1000,
+        backgroundcolor: '#000',
+        overlayOpacity:    0.6,
+        position:   'center',
+        cancel:  undefined
+    });
+
+});
