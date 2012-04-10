3f4f38925c2e783954a41ae5315e56f12b4edf4b
liuyonghui <sdjllyh@gmail.com>
2012-04-10 14:00:58 +0800

    修改switch.js的参数,添加选中的class

--- fx/switch.js
+++ fx/switch.js
@@ -2,7 +2,7 @@
  * Example 1:
  * <* fx="switch[switchid=div1]" >1</*>
  * <* fx="switch[event=mouseover;switchid=div2]" >1</*>
- * <* fx="switch[event=mouseover;switchclass=c1;nochangeclass]" >1</*>
+ * <* fx="switch[event=mouseover;switchClass=c1;]" >1</*>
  *
  * <* id="div1">content1</*>
  * <* id="div2">content2</*>
@@ -12,12 +12,14 @@
  * <* fx="switch[switchid=diva;group=some]" >1</*>
  * <* id="diva">content1</*>
  *
- * 必须有switchid或switchclass中的一个或两个
+ * 必须有switchid或switchClass中的一个或两个
  * 多个分组时用group参数
  * event可以是mouseover mouseout click 等事件
  * 如果IE6 下出现页面跳动, 给被switch的*们再包一个div, 然后让这个div的高宽固定
  *
  * */
+
+// todo  switch应该把switchid改为选择器
 
 
 ZARK_FX.getFrame('jquery-1.3.2', function($){
@@ -26,23 +28,24 @@ ZARK_FX.getFrame('jquery-1.3.2', function($){
     ZARK_FX.run('switch', function(attrs){
 
         var $this = $(this);
-        var group = attrs.group !== undefined? attrs.group : 'default';
+        var group = attrs.group !== undefined ? attrs.group : 'default_group';
         var switchFunction = function(){
             for(var i in switch_groups[group]){
                 var $a = switch_groups[group][i];
                 var hide_id = ZARK_FX.parseFX($a.attr(ZARK_FX.FX_NAME))['switch'].switchid;
-                var hide_cl = ZARK_FX.parseFX($a.attr(ZARK_FX.FX_NAME))['switch'].switchclass;
+                var hide_cl = ZARK_FX.parseFX($a.attr(ZARK_FX.FX_NAME))['switch'].switchClass;
                 if (hide_id !== undefined) $('#'+hide_id).hide();
                 if (hide_cl !== undefined) $('.'+hide_cl).hide();
-                $a.addClass('zarkfx_unchosed').removeClass('zarkfx_chosed');
+                $a.addClass(attrs.unselectedClass).removeClass(attrs.selectedClass);
             };
             var show_id = attrs.switchid;
-            var show_cl = attrs.switchclass;
+            var show_cl = attrs.switchClass;
             if (show_id !== undefined) $('#'+show_id).show();
             if (show_cl !== undefined) $('.'+show_cl).show();
-            if (attrs.nochangeclass === undefined) $this.addClass('zarkfx_chosed').removeClass('zarkfx_unchosed');
+            $this.addClass(attrs.selectedClass).removeClass(attrs.unselectedClass);
             if ($this.attr('nodeName') === 'A') {
                 $this.blur();
+                return false;
             };
         };
 
@@ -62,22 +65,36 @@ ZARK_FX.getFrame('jquery-1.3.2', function($){
         if (attrs.autoHidden){
             // show the first switched, and hide others
             if (switch_groups[group].length == 1){
-                if (attrs.nochangeclass === undefined) $this.addClass('zarkfx_chosed').removeClass('zarkfx_unchosed');
                 if (attrs.switchid !== undefined) $('#'+attrs.switchid).show();
+                if (attrs.switchClass !== undefined) $('.'+attrs.switchClass).show();
             }else{
                 if (attrs.switchid !== undefined) $('#'+attrs.switchid).hide();
-                if (attrs.switchclass !== undefined) $('.'+attrs.switchclass).hide();
+                if (attrs.switchClass !== undefined) $('.'+attrs.switchClass).hide();
             };
 
             // change the first switcher's class
             if (switch_groups[group].length == 1){
-                if (attrs.switchclass !== undefined) $('.'+attrs.switchclass).show();
+                if (attrs.switchid !== undefined) {
+                    $('#' + attrs.switchid).addClass(attrs.selectedClass);
+                };
+                if (attrs.switchClass !== undefined) {
+                    $('.' + attrs.switchClass).addClass(attrs.selectedClass);
+                };
             }else{
-                if (attrs.nochangeclass === undefined) $(this).addClass('zarkfx_unchosed').removeClass('zarkfx_chosed');
+                if (attrs.switchid !== undefined) {
+                    $('#' + attrs.switchid).addClass(attrs.unselectedClass);
+                };
+                if (attrs.switchClass !== undefined) {
+                    $('.' + attrs.switchClass).addClass(attrs.unselectedClass);
+                };
             };
         };
 
     }, {
-        autoHidden: true
+        switchid:       undefined,
+        switchClass:    undefined,
+        autoHidden:     true,
+        selectedClass:  'zarkfx_switch_selected',
+        unselectedClass:'zarkfx_switch_unselected'
     });
 });
