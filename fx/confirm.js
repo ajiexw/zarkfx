4d8e701c831e7b298d7bebbc393f727045fbcdb4
liuyonghui <sdjllyh@gmail.com>
2012-04-18 13:38:11 +0800

    bug

--- fx/confirm.js
+++ fx/confirm.js
@@ -1,10 +1,23 @@
 ZARK_FX.getFrame('jquery-1.3.2', function($) {
 
     ZARK_FX.run('confirm', function(attrs) {
+        var $this = $(this);
 
-        $(this).click(function(){
-            return confirm(attrs.msg);
-        });
+        if ($this.attr('onclick')){
+            var old_click = $this.attr('onclick');
+            $this.removeAttr('onclick').click(function(){
+                if ( confirm(attrs.msg) ){
+                    old_click && old_click.call(this);
+                    return true;
+                }else{
+                    return false;
+                };
+            });
+        }else{
+            $this.click(function(){
+                return confirm(attrs.msg);
+            });
+        };
 
     }, {
         style:      'none',
