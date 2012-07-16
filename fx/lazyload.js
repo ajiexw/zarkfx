8dc6415b06d080583a833df946f7890952a515ef
liuyonghui <sdjllyh@gmail.com>
2012-07-16 13:10:34 +0800

    test

--- fx/lazyload.js
+++ fx/lazyload.js
@@ -4,20 +4,13 @@ FX.getFrame('jquery-1.5.1', function($){
 
     FX.run('lazyload', function(attrs){
 
-        var $this = $(this);
-
-        //在lazyload中, effect=show和effectspeed不能同时使用
-        if (attrs.effect !== 'show'){
-            attrs.effectspeed = attrs.effectSpeed;
-        };
-        delete attrs.effectSpeed;
-
-        $this.lazyload(attrs);
     
     }, {
         skip_invisible: false,
         effectSpeed: 1000,
         effect:     'show' // 可选参数: show fadeIn
     }, 'lazyload' );
+
+    $('img[fx*=lazyload]').lazyload({skip_invisible:false, effect='show'});
 });
 
