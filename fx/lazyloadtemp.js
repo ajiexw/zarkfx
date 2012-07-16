f86eee26396efe8ec6e0cf6d09c85d4ef29d9c0c
liuyonghui <sdjllyh@gmail.com>
2012-07-16 13:40:53 +0800

    add fx/lazyloadtemp.js

---
+++ fx/lazyloadtemp.js
@@ -0,0 +1,14 @@
+// lazyload.js 不支持 jquery-.1.3.2
+
+FX.getFrame('jquery-1.5.1', function($){
+    var first = true;
+
+    FX.run('lazyload', function(attrs){
+        if (first){
+            $('img[fx*=lazyload]').lazyload({skip_invisible:false, effect:'show'});
+            first = false;
+        }
+    }, {
+    }, 'lazyload' );
+});
+
