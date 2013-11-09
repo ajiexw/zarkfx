39b22f280cb1401c96bc56b4e47ee43e91fcd932
ajie <369520295@qq.com>
2013-11-09 14:23:52 +0800

    none

--- fx/richtext.js
+++ fx/richtext.js
@@ -12,12 +12,14 @@ FX.getFrame('jquery-1.3.2', function($){
             plugins : "autolink,lists,style,iespell,media,fullscreen,template,advlist,sparker5image,table,searchreplace,advhr,directionality,preview,",
 
             // Theme options
-            theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,sparker5image,code",
-            theme_advanced_buttons2 : "tablecontrols,|,search,replace,|,bullist,numlist,outdent,indent,forecolor,backcolor,removeformat,|,preview,hr,advhr,|,ltr,rtl,|,undo,redo,link,unlink,image,charmap,media,styleprops,template,|,fullscreen", 
+            //theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,sparker5image,code",
+            theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,fontsizeselect,forecolor,backcolor,styleprops,removeformat,|,code,hr,link,unlink,undo,redo,sparker5image",
+            //theme_advanced_buttons2 : "tablecontrols,|,search,replace,|,bullist,numlist,outdent,indent,forecolor,backcolor,removeformat,|,preview,hr,advhr,|,ltr,rtl,|,undo,redo,link,unlink,image,charmap,media,styleprops,template,|,fullscreen", 
+            theme_advanced_buttons2 : "",
             theme_advanced_buttons3 : "",
             theme_advanced_toolbar_location : "top",
             theme_advanced_toolbar_align : "left",
-            theme_advanced_statusbar_location : "bottom",
+            //theme_advanced_statusbar_location : "bottom",
             theme_advanced_resizing : true,
             relative_urls : false, 
             theme_advanced_font_sizes : "10px,11px,12px,13px,14px,16px,18px,20px,24px",
